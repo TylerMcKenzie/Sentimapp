72bbe5e621bebc45b70fb84b5035389a334b0169
tylermckenzie <tylrmckenz@gmail.com>
2017-01-03 02:42:57 -0600

    add welcome to tutorial, looks nice, needs polish

--- app/assets/javascripts/factories/tutorial.js
+++ app/assets/javascripts/factories/tutorial.js
@@ -1,5 +1,6 @@
 app.factory("Tutorial", [
-  function() {
+  'Scroller',
+  function(Scroller) {
     // set tutorial global
     var tutAnimation;
 
@@ -10,6 +11,7 @@ app.factory("Tutorial", [
       var fullJaw = sentibot.find('#full_jaw');
       var jaw = sentibot.find('#jaw');
       var jawL = sentibot.find('#jaw_lights');
+      var jawLG = sentibot.find('#jaw_light_glow');
       var leftN = sentibot.find('#nut_left');
       var rightN = sentibot.find('#nut_right');
       var eyes = sentibot.find('#eyes');
@@ -19,13 +21,14 @@ app.factory("Tutorial", [
       
       // postitioning vars
       var $window = $(window);
+      var CPlocation = sentibot.offset().top+sentibot.height();
 
       // provide way to set prompt text
       function setPrompter(sceneNumber, locationX, locationY, display) {
         // script for tutorial
         var script = [
                       'Welcome to Sentimapp, the sentiment analysis application.',
-                      'I am Sentibot, I\'ll be your guide for this app.',
+                      'I am Sentibot, I\'ll be your guide for this experience.',
                       'Let\'s take a look at our controls.'
                     ];
 
@@ -52,12 +55,18 @@ app.factory("Tutorial", [
         prompter.hide();
       };
 
-      setPrompter(0, ($window.width()/1.5), 50, 'show');
-      
+      // function to move body to control panel
+      function scroll(location, duration) {
+        Scroller.scrollTo(location, duration);
+      };
+
       // Main timeline
       tutAnimation = new TimelineLite({paused:true});
+      // tutAnimation = new TimelineLite();
       
       // animations
+    
+      // 0.75 seconds
       function jawBob() {
         var jawBob = new TimelineLite();
         
@@ -66,9 +75,11 @@ app.factory("Tutorial", [
               .to(fullJaw, 0.1875, {y: 10})
               .to(fullJaw, 0.1875, {y: 0});
 
+
         return jawBob;
       };
 
+      // 0.60 seconds
       function eyeFlicker() {
         var eyeFlicker = new TimelineLite();
 
@@ -81,13 +92,39 @@ app.factory("Tutorial", [
       };
 
       // scenes
-      function scene1() {
+      function welcome() {
+        // set timeline
+        var tl = new TimelineLite();
 
+        tl.call(setPrompter, [0, $window.width()/1.5, 75, 'show']);
+        tl.add('talk');
+        tl.add(jawBob, 'talk');
+        tl.add('flicker');
+        tl.add(eyeFlicker, 'flicker');
+        tl.add('talk2', "+=2");
+        tl.add(jawBob, 'talk2');
+        tl.add('flicker2');
+        tl.add(eyeFlicker, 'flicker2');
+        tl.call(setPrompter, [1, $window.width()/1.5, 75, 'show'], this, 'talk2');
+        tl.add('talk3', "+=2");
+        tl.add(jawBob, 'talk3');
+        tl.add('flicker3');
+        tl.add(eyeFlicker, 'flicker3');
+        tl.call(setPrompter, [2, $window.width()/1.5, 75, 'show'], this, 'talk3');
+        tl.add('removePromp', '+=1');
+        tl.call(setPrompter, [0, $window.width()/1.5, 75, 'hide'], this, 'removePromp');
+        tl.add('goto');
+        tl.call(scroll, [CPlocation, 1500], this, 'goto');
+
+        return tl;
       };
+
 
 
       // Add scenes to timeline (--rough first then seek in app--)
       
+      tutAnimation.add('welcome')
+      tutAnimation.add(welcome, 'welcome');
 
     };
 
@@ -96,11 +133,11 @@ app.factory("Tutorial", [
     };
 
     Tutorial.prototype.playScene = function(scene) {
-
+      tutAnimation.seek(scene);
     };
 
     Tutorial.prototype.stopTutorial = function() {
-
+      tutAnimation.kill();
     };
 
 
